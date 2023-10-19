85cf512a96c747d8dad23b669f437325241b93ee
Tiga <sawadogotiga98@gmail.com>
2023-10-19 21:55:28 +0200

    variables end

--- booking_app/main.go
+++ booking_app/main.go
@@ -4,14 +4,28 @@ import "fmt"
 
 func main() {
 
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	var reminingTickets = 50
+	conferenceName := "Go conference"
+	const conferenceTickets int = 50
+	var reminingTickets int = 50
 
-	fmt.Println("Welcome to", conferenceName, "Booking application")
-	fmt.Println("We have total of", conferenceTickets, "tickets and", reminingTickets)
+	fmt.Printf("Welcome to %v booking application\n", conferenceName)
+
+	fmt.Printf("We have total of %v tickets and %v are still available\n", conferenceTickets, reminingTickets)
 	fmt.Println("Get your ticket here to attend.")
+	fmt.Printf("conferenceName is %T, conferenceTickets is %T, remindingTickers is %T \n", conferenceName, conferenceTickets, reminingTickets)
 
-	fmt.Println("Here we go with variables")
+	var userName string
+	var userTickets int
+
+	userName = "Tyga"
+	userTickets = 40
+	fmt.Printf("user %v booked %v tickets.\n", userName, userTickets)
+
+	//// Println
+	// fmt.Println("Welcome to", conferenceName, "Booking application")
+	// fmt.Println("We have total of", conferenceTickets, "tickets and", reminingTickets)
+	// fmt.Println("Get your ticket here to attend.")
+
+	// fmt.Println("Here we go with variables")
 
 }
